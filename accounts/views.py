0539af7501704c3ced805c415e1bcdc865c6a4ce
Trump4President2020 <36775940+Trump4President2020@users.noreply.github.com>
2021-01-07 12:09:52 +0100

    fixed typo

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('url_home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'signup_form': form})
